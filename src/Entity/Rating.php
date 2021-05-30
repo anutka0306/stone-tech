f84f47892712a354554455a5c25f266fe0c8eab0
anutka0306 <anutka0306@mail.ru>
2021-05-30 19:16:10 +0300

    Вывод преимуществ и блока с рейтингом

---
+++ src/Entity/Rating.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RatingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RatingRepository::class)
+ */
+class Rating
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getCount(): ?int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): self
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+}
---
+++ src/Widget/Rating.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace App\Widget;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\RatingRepository;
+
+
+class Rating extends AbstractController
+{
+
+    public function show(RatingRepository $ratingRepository){
+
+        $rating = array();
+        $rating['result'] = $ratingRepository->findOneBy(['path'=>$_SERVER['REQUEST_URI']]);
+        $rating['stars_html'] = $this->renderStars($rating['result']->getValue());
+        if (null === $rating) {
+            return;
+        }
+        $ratingItems = [
+            [
+                'link' => 'https://kamennye-tehnologii.blizko.ru/reviews',
+                'img'=> 'images/blizko.jpg',
+                'alt'=> 'Близко.ру',
+                'stars'=> 5.0,
+                'rating'=> 5.0
+            ],
+            [
+                'link' => 'https://yandex.ru/maps/org/kamtekh/1118245677/?ll=37.751269%2C55.762738&z=14',
+                'img'=> 'images/rating-logo-yandex.png',
+                'alt'=> 'Yandex',
+                'stars'=> 4.2,
+                'rating'=> 4.2
+            ],
+            [
+                'link' => 'https://goo.gl/maps/PrAZMvq9TgVKp4dC7',
+                'img'=> 'images/rating-logo-google.png',
+                'alt'=> 'Google',
+                'stars'=> 5,
+                'rating'=> 5.0
+            ],
+            [
+                'link' => 'https://go.2gis.com/caj03f',
+                'img'=> 'images/rating-logo-2gis.png',
+                'alt'=> '2 Gis',
+                'stars'=> 5.0,
+                'rating'=> 5.0
+            ],
+        ];
+
+        foreach ($ratingItems as $key =>$item){
+            $ratingItems[$key]['stars_html'] = $this->renderStars($item['stars']);
+        }
+        return $this->render('widgets/rating.html.twig', compact('ratingItems', 'rating'));
+    }
+
+    private function renderStars($value, $max=5)
+    {
+        $html = '<div class="stars">';
+        for ($i=1;$i<=$max;$i++){
+            $fill_class = '';
+            if ($i <= $value ) {
+                $fill_class = 'on';
+            }
+            elseif ($i > $value && $i-1 < $value){
+                $fill_class = 'half';
+            }
+
+            $html .='<span class="star '.$fill_class.'"></span>';
+        }
+        $html .='</div>';
+        return $html;
+    }
+
+}
