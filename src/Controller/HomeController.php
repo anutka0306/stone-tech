6dd8f1155e4392d8147a761551dfc52564bce045
Anna Kotelnikova <anutka0306@mail.ru>
2021-07-22 18:07:04 +0300

    пробелы у мета тегов

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ContentRepository;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @var ContentRepository
+     */
+    protected $page_repository;
+
+    public function __construct(ContentRepository $repository){
+        $this->page_repository = $repository;
+    }
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        if(!$page = $this->page_repository->findOneBy(['path'=>'/']) ){
+            throw $this->createNotFoundException('Main page not found');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'page' => $page,
         ]);
     }
 }
